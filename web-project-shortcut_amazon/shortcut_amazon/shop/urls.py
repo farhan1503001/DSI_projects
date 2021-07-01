eb33fd84936bc945754b43bbd7d20a81338f99d0
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-07-01 17:11:29 +0600

    adding urlpattern

--- web-project-shortcut_amazon/shortcut_amazon/shop/urls.py
+++ web-project-shortcut_amazon/shortcut_amazon/shop/urls.py
@@ -2,7 +2,10 @@
 from django.urls.conf import include
 from .views import cart, home
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
   path('',home,name='main'),
   path('home/cart',cart,name='shopping'),
 ]
+
+urlpatterns+=staticfiles_urlpatterns()
